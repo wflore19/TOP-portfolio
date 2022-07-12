0b73e0762534502a121db860f2c56dab00725081
Will Flores <willflores1221@gmail.com>
2022-07-12 01:57:01 -0400

    Add scroll into view function

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,14 +1,50 @@
 import './navbar.css';
 
-export default function NavBar() {
-  return (
+function NavBar() {
+
+  function handleLinkTouch(e: any) {
+    if (e.target.textContent === "Home") {
+      // @ts-ignore: Object is possibly 'null'.
+      document
+        .querySelector(".heroes")
+        .scrollIntoView({behavior:"smooth"});
+    } else if(e.target.textContent === "About") {
+      // @ts-ignore: Object is possibly 'null'.
+      document
+        .querySelector(".whoami")
+        .scrollIntoView({behavior:"smooth"}); 
+    } else if (e.target.textContent === "Skills"){
+      // @ts-ignore: Object is possibly 'null'.
+      document
+        .querySelector(".skills")
+        .scrollIntoView({behavior:"smooth"});
+    } else if (e.target.textContent === "Projects"){
+      // @ts-ignore: Object is possibly 'null'.
+      document
+        .querySelector(".projects")
+        .scrollIntoView({behavior:"smooth"});
+    } else if (e.target.textContent === "Resume"){
+      // @ts-ignore: Object is possibly 'null'.
+      document
+        .querySelector(".projects")
+        .scrollIntoView({behavior:"smooth"});
+    } else if (e.target.textContent === "Contact"){
+      // @ts-ignore: Object is possibly 'null'.
+      document
+        .querySelector(".whoami")
+        .scrollIntoView({behavior:"smooth"});
+    }
+  }
+  
+    return (
     <div id="nav-group">
-      <div className="nav-item" >Home</div>
-      <div className="nav-item" >About</div>
-      <div className="nav-item" >Skills</div>
-      <div className="nav-item" >Projects</div>
-      <div className="nav-item" >Resume</div>
-      <div className="nav-item" >Contact</div>
+      <button className="nav-item" onClick={handleLinkTouch}>Home</button>
+      <button className="nav-item" onClick={handleLinkTouch}>About</button>
+      <button className="nav-item" onClick={handleLinkTouch}>Skills</button>
+      <button className="nav-item" onClick={handleLinkTouch}>Projects</button>
+      <button className="nav-item" onClick={handleLinkTouch}>Resume</button>
+      <button className="nav-item" onClick={handleLinkTouch}>Contact</button>
     </div>
   )
 }
+export default NavBar;
