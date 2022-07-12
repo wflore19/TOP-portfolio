8b98f7ea1feee589b042a527864743a191702d91
Will Flores <willflores1221@gmail.com>
2022-07-12 01:54:30 -0400

    Init resume linkedin link

--- src/components/Heroes/Heroes.tsx
+++ src/components/Heroes/Heroes.tsx
@@ -6,8 +6,8 @@ export default function Heroes() {
     <div className="heroes">
       {/* <img src={imposter} alt="img" className="copy" /> */}
       <div className='btns-list'>
-        <a className="btns" href="https://github.com/will-flores1" target="_blank" rel="noreferrer">Resume</a>
-        <a className="btns" href="https://github.com/will-flores1" target="_blank" rel="noreferrer">Project</a>
+        <a className="btns" href="https://docs.google.com/document/d/1YgyqVlHHdGI0bW5oVdtBjlB4vu_UHn_edhggokYSOIM/edit?usp=sharing" target="_blank" rel="noreferrer">Resume</a>
+        <a className="btns" href="https://www.linkedin.com/in/wilfredo-flores-666492173/" target="_blank" rel="noreferrer">LinkedIn</a>
         <a className="btns" href="https://github.com/will-flores1" target="_blank" rel="noreferrer">Github</a>
         <a className="btns" href="https://www.youtube.com/c/WillFlores1" target="_blank" rel="noreferrer">Youtube</a>
       </div>
