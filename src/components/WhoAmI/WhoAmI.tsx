ae7e2f845bb066bf2f658d6895dd3900d5434715
Will Flores <willflores1221@gmail.com>
2022-07-15 19:20:51 -0400

    Edit summary

--- src/components/WhoAmI/WhoAmI.tsx
+++ src/components/WhoAmI/WhoAmI.tsx
@@ -1,10 +1,36 @@
-import './whoami.css';
+import "./whoami.css";
 
 export default function WhoAmI() {
-  return (
-    <div className='whoami'>Who Am I<span style={{color: 'white', backgroundColor: '#2D3047', borderRadius: 4, padding: 3, margin: 3}}>?</span>
-      <div className='text'>I started programming with C++/Java in introduction courses at Montgomery College back in 2018. In 2022 I started learning web development on my own, I built applications using Javascript, HTML, CSS, and Python. I worked through The Odin Project's entire full-stack JavaScript boot camp while working full-time at an autobody shop. I am now set on contributing to the success of a professional development team. Through my time spent teaching myself to code, I have developed discipline, curiosity, and passion. It has been a wonderful experience, and I am excited to continue learning every day in a professional environment. If you would like to get in touch, please e-mail me at <mark className='highlight'>wflore19@montgomerycollege.edu</mark>. You might also be interested in taking a look at my full array of skills, as well as some of the projects I have completed by scrolling down this page.
-      </div>
-    </div>
-  )
+	return (
+		<div className="whoami">
+			Who Am I
+			<span
+				style={{
+					color: "white",
+					backgroundColor: "var(--primary-color)",
+					borderRadius: 4,
+					padding: 3,
+					margin: 3,
+				}}
+			>
+				?
+			</span>
+			<div className="text">
+				I started programming with C++/Java in introduction courses at
+				Montgomery College back in 2018. In 2022 I started learning web
+				development on my own, I built applications using Javascript, HTML, CSS,
+				and Python. I worked through The Odin Project's entire full-stack
+				JavaScript boot camp while working full-time at an autobody shop. I am
+				now set on contributing to the success of a professional development
+				team. Through my time spent teaching myself to code, I have developed
+				discipline, curiosity, and passion. It has been a wonderful experience,
+				and I am excited to continue learning every day in a professional
+				environment. If you would like to get in touch, please e-mail me at{" "}
+				<mark className="highlight">wflore19@montgomerycollege.edu</mark>. You
+				might also be interested in taking a look at my full array of skills, as
+				well as some of the projects I have completed by scrolling down this
+				page.
+			</div>
+		</div>
+	);
 }
