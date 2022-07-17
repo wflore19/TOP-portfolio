9c3cecc1f36be983e9509e52a7efa71432666b68
Will Flores <willflores1221@gmail.com>
2022-07-17 18:30:38 -0400

    Add experience scroll function

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,44 +1,47 @@
-import './navbar.css';
+import "./navbar.css";
 
 function NavBar() {
+	function handleLinkTouch(e: any) {
+		if (e.target.textContent === "Home") {
+			document.querySelector(".heroes")!.scrollIntoView({ behavior: "smooth" });
+		} else if (e.target.textContent === "About") {
+			document.querySelector(".whoami")!.scrollIntoView({ behavior: "smooth" });
+		} else if (e.target.textContent === "Skills") {
+			document.querySelector(".skills")!.scrollIntoView({ behavior: "smooth" });
+		} else if (e.target.textContent === "Projects") {
+			document
+				.querySelector(".projects")!
+				.scrollIntoView({ behavior: "smooth" });
+		} else if (e.target.textContent === "Experience") {
+			document
+				.querySelector(".experience")!
+				.scrollIntoView({ behavior: "smooth" });
+		} else if (e.target.textContent === "Contact") {
+			document.querySelector(".whoami")!.scrollIntoView({ behavior: "smooth" });
+		}
+	}
 
-  function handleLinkTouch(e: any) {
-    if (e.target.textContent === "Home") {
-      document
-        .querySelector(".heroes")!
-        .scrollIntoView({behavior:"smooth"});
-    } else if(e.target.textContent === "About") {
-      document
-        .querySelector(".whoami")!
-        .scrollIntoView({behavior:"smooth"}); 
-    } else if (e.target.textContent === "Skills"){
-      document
-        .querySelector(".skills")!
-        .scrollIntoView({behavior:"smooth"});
-    } else if (e.target.textContent === "Projects"){
-      document
-        .querySelector(".projects")!
-        .scrollIntoView({behavior:"smooth"});
-    } else if (e.target.textContent === "Resume"){
-      document
-        .querySelector(".projects")!
-        .scrollIntoView({behavior:"smooth"});
-    } else if (e.target.textContent === "Contact"){
-      document
-        .querySelector(".whoami")!
-        .scrollIntoView({behavior:"smooth"});
-    }
-  }
-  
-    return (
-    <div id="nav-group">
-      <button className="nav-item" onClick={handleLinkTouch}>Home</button>
-      <button className="nav-item" onClick={handleLinkTouch}>About</button>
-      <button className="nav-item" onClick={handleLinkTouch}>Skills</button>
-      <button className="nav-item" onClick={handleLinkTouch}>Projects</button>
-      <button className="nav-item" onClick={handleLinkTouch}>Resume</button>
-      <button className="nav-item" onClick={handleLinkTouch}>Contact</button>
-    </div>
-  )
+	return (
+		<div id="nav-group">
+			<button className="nav-item" onClick={handleLinkTouch}>
+				Home
+			</button>
+			<button className="nav-item" onClick={handleLinkTouch}>
+				About
+			</button>
+			<button className="nav-item" onClick={handleLinkTouch}>
+				Skills
+			</button>
+			<button className="nav-item" onClick={handleLinkTouch}>
+				Projects
+			</button>
+			<button className="nav-item" onClick={handleLinkTouch}>
+				Experience
+			</button>
+			<button className="nav-item" onClick={handleLinkTouch}>
+				Contact
+			</button>
+		</div>
+	);
 }
 export default NavBar;
