feafaa2eec5e4e4be70e6d109999eeb376fe7066
Will Flores <willflores1221@gmail.com>
2022-07-17 18:30:19 -0400

    Add experiences components

--- src/components/Mainframe/Mainframe.tsx
+++ src/components/Mainframe/Mainframe.tsx
@@ -1,16 +1,18 @@
-import './mainframe.css';
-import Heroes from '../Heroes/Heroes';
-import WhoAmI from '../WhoAmI/WhoAmI';
-import Skills from '../Skills/Skills';
-import Projects from '../Projects/Projects';
+import "./mainframe.css";
+import Heroes from "../Heroes/Heroes";
+import WhoAmI from "../WhoAmI/WhoAmI";
+import Skills from "../Skills/Skills";
+import Projects from "../Projects/Projects";
+import Experiences from "../Experience/Experiences";
 
 export default function Mainframe() {
-  return (
-    <div className="Mainframe">
-      <Heroes />
-      <WhoAmI />
-      <Skills />
-      <Projects />
-    </div>
-  )
+	return (
+		<div className="Mainframe">
+			<Heroes />
+			<WhoAmI />
+			<Skills />
+			<Projects />
+			<Experiences />
+		</div>
+	);
 }
