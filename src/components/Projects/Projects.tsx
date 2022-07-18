02ab8cfbd877f4629707b09578700a31a12e0800
Will Flores <willflores1221@gmail.com>
2022-07-18 13:57:00 -0400

    Update project stack

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -19,7 +19,7 @@ export default function Projects() {
 			<Project
 				logo="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Twitter-logo.svg/934px-Twitter-logo.svg.png"
 				title="Twitter 2.0"
-				madeWith="Next.js, React, Typescript, Sanity, Tailwindcss"
+				madeWith="Next.js, React, Typescript, Sanity, GROQ, Tailwindcss"
 				description="I built this project as a intro to auth and data fetching. Clone of the popular social app Twitter. It includes tweets, sign in, and comment funtionality."
 				gif="https://media.giphy.com/media/tSOPUnPzbxAUVZsUjt/giphy.gif"
 				demo="https://twitter-2-0-nu.vercel.app/"
@@ -28,7 +28,7 @@ export default function Projects() {
 			<Project
 				logo="https://cdn4.iconfinder.com/data/icons/social-media-circle-7/512/Medium_circle-512.png"
 				title="Medium 2.0"
-				madeWith="Next.js, React, Typescript, Sanity, Tailwindcss"
+				madeWith="Next.js, React, Typescript, Sanity, GROQ,  Tailwindcss"
 				description="Clone of the popular blogging app Medium. The purpose of this project is to practice server-side rendering, data fetching, and form validation with Next.js and Sanity CMS. Deployed with Vercel."
 				gif="https://media.giphy.com/media/meblGF6wWnHssANkdg/giphy.gif"
 				demo="https://medium-2-0-sooty.vercel.app/"
