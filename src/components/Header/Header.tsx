977675c68134cb33f26ea36d6c9d0d1fc4834cf9
Will Flores <willflores1221@gmail.com>
2022-07-20 12:05:11 -0400

    Add component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,24 @@
-import './header.css';
+import Hamburger from "../Hamburger/Hamburger";
+import "./header.css";
 
 export default function Header() {
-  return (
-    <header>
-      <div id='header'>wilfredo_flores()<span style={{backgroundColor: '#2D3047', borderRadius: 4, color: 'white', padding: 3, margin: 3}}>;</span></div>
-    </header>
-  )
+	return (
+		<header>
+			<div id="header">
+				wilfredo_flores()
+				<span
+					style={{
+						backgroundColor: "#2D3047",
+						borderRadius: 4,
+						color: "white",
+						padding: 3,
+						margin: 3,
+					}}
+				>
+					;
+				</span>
+			</div>
+			<Hamburger />
+		</header>
+	);
 }
