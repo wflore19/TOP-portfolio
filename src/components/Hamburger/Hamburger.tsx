22e50f78cd31ec0f900ea23c052895c67ef79ba0
Will Flores <willflores1221@gmail.com>
2022-07-20 12:04:59 -0400

    Remove hamburger menu

--- src/components/Hamburger/Hamburger.tsx
+++ src/components/Hamburger/Hamburger.tsx
@@ -1,68 +1,55 @@
-import './hamburger.css';
+import "./hamburger.css";
+import { MenuIcon } from "@heroicons/react/solid";
 
 export default function Hamburger() {
-  function handleLinkTouch(e: any) {
-    if (e.target.textContent === "Home") {
-      // @ts-ignore: Object is possibly 'null'.
-      document
-        .querySelector(".heroes")
-        .scrollIntoView({behavior:"smooth"});
-    } else if(e.target.textContent === "About") {
-      // @ts-ignore: Object is possibly 'null'.
-      document
-        .querySelector(".whoami")
-        .scrollIntoView({behavior:"smooth"}); 
-    } else if (e.target.textContent === "Skills"){
-      // @ts-ignore: Object is possibly 'null'.
-      document
-        .querySelector(".skills")
-        .scrollIntoView({behavior:"smooth"});
-    } else if (e.target.textContent === "Projects"){
-      // @ts-ignore: Object is possibly 'null'.
-      document
-        .querySelector(".projects")
-        .scrollIntoView({behavior:"smooth"});
-    } else if (e.target.textContent === "Resume"){
-      // @ts-ignore: Object is possibly 'null'.
-      document
-        .querySelector(".projects")
-        .scrollIntoView({behavior:"smooth"});
-    } else if (e.target.textContent === "Contact"){
-      // @ts-ignore: Object is possibly 'null'.
-      document
-        .querySelector(".whoami")
-        .scrollIntoView({behavior:"smooth"});
-    }
-  }
+	function handleLinkTouch(e: any) {
+		if (e.target.textContent === "Home") {
+			// @ts-ignore: Object is possibly 'null'.
+			document.querySelector(".heroes").scrollIntoView({ behavior: "smooth" });
+		} else if (e.target.textContent === "About") {
+			// @ts-ignore: Object is possibly 'null'.
+			document.querySelector(".whoami").scrollIntoView({ behavior: "smooth" });
+		} else if (e.target.textContent === "Skills") {
+			// @ts-ignore: Object is possibly 'null'.
+			document.querySelector(".skills").scrollIntoView({ behavior: "smooth" });
+		} else if (e.target.textContent === "Projects") {
+			// @ts-ignore: Object is possibly 'null'.
+			document
+				.querySelector(".projects")
+				.scrollIntoView({ behavior: "smooth" });
+		} else if (e.target.textContent === "Resume") {
+			// @ts-ignore: Object is possibly 'null'.
+			document
+				.querySelector(".experience")
+				.scrollIntoView({ behavior: "smooth" });
+		} else if (e.target.textContent === "Contact") {
+			// @ts-ignore: Object is possibly 'null'.
+			document.querySelector(".whoami").scrollIntoView({ behavior: "smooth" });
+		}
+	}
 
-  const hamburger = document.querySelector(".hamburger")!;
-  const navMenu = document.querySelector(".nav-menu")!;
-
-  hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-  });
-
-  document.querySelectorAll(".nav-menu").forEach(n => n.addEventListener('click', () => {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-  }));
-
-  return (
-    <nav className="navbar">
-      <div className="nav-menu">
-        <button className="nav-item" onClick={handleLinkTouch}>Home</button>
-        <button className="nav-item" onClick={handleLinkTouch}>About</button>
-        <button className="nav-item" onClick={handleLinkTouch}>Skills</button>
-        <button className="nav-item" onClick={handleLinkTouch}>Projects</button>
-        <button className="nav-item" onClick={handleLinkTouch}>Resume</button>
-        <button className="nav-item" onClick={handleLinkTouch}>Contact</button>
-      </div>
-      <div className="hamburger">
-        <span className="bar"></span>
-        <span className="bar"></span>
-        <span className="bar"></span>
-      </div>
-    </nav>
-  )
+	return (
+		<nav className="navbarsm">
+			<div className="nav-menusm">
+				<button className="nav-itemsm" onClick={handleLinkTouch}>
+					Home
+				</button>
+				<button className="nav-itemsm" onClick={handleLinkTouch}>
+					About
+				</button>
+				<button className="nav-itemsm" onClick={handleLinkTouch}>
+					Skills
+				</button>
+				<button className="nav-itemsm" onClick={handleLinkTouch}>
+					Projects
+				</button>
+				<button className="nav-itemsm" onClick={handleLinkTouch}>
+					Resume
+				</button>
+				<button className="nav-itemsm" onClick={handleLinkTouch}>
+					Contact
+				</button>
+			</div>
+		</nav>
+	);
 }
