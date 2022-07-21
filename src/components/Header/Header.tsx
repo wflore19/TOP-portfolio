312bbccf958a2e2cef45d74d4d05f817f2509c24
Will Flores <willflores1221@gmail.com>
2022-07-21 19:00:31 -0400

    Edit header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,22 @@
-import './header.css';
+import "./header.css";
 
 export default function Header() {
-  return (
-    <header>
-      <div id='header'>wilfredo_flores()<span style={{backgroundColor: '#2D3047', borderRadius: 4, color: 'white', padding: 3, margin: 3}}>;</span></div>
-    </header>
-  )
+	return (
+		<header>
+			<div id="header">
+				&lt;wilfredo_flores /&gt;
+				<span
+					style={{
+						backgroundColor: "#2D3047",
+						borderRadius: 4,
+						color: "white",
+						padding: 3,
+						margin: 3,
+					}}
+				>
+					;
+				</span>
+			</div>
+		</header>
+	);
 }
