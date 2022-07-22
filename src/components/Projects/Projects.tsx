c4b78d91b08107ca823b919be010ce37a7f43448
Will Flores <willflores1221@gmail.com>
2022-07-21 22:08:21 -0400

    Add portfolios

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,5 +1,6 @@
 import Project from "./Project";
 import "./projects.css";
+import pp from "../assets/profile-picture.png";
 
 export default function Projects() {
 	return (
@@ -33,6 +34,24 @@ export default function Projects() {
 				gif="https://media.giphy.com/media/meblGF6wWnHssANkdg/giphy.gif"
 				demo="https://medium-2-0-sooty.vercel.app/"
 				github="https://github.com/will-flores1/medium-2.0"
+			/>
+			<Project
+				logo={pp}
+				title="Portfolio"
+				madeWith="React, Typescript, Tailwindcss"
+				description="My first portfolio. Functional scroll to NavBar, responsive design, pure CSS, and pizzazz."
+				gif="https://media.giphy.com/media/AYziFcIOwC3HEmv2HC/giphy.gif"
+				demo="https://www.wilfredoflores.dev/"
+				github="https://github.com/will-flores1/portfolio"
+			/>
+			<Project
+				logo={pp}
+				title="Minimalist Portfolio"
+				madeWith="React, Typescript, Tailwindcss"
+				description="Minimal portfolio style. Practice for responsive design, tailwindcss, and themes. Inpired by Takuya Matsuyama"
+				gif="https://media.giphy.com/media/ZjCBYlg1uDtdUdbeYr/giphy.gif"
+				demo="https://portfolio-too.vercel.app/"
+				github="https://github.com/will-flores1/portfolio-too"
 			/>
 		</div>
 	);
