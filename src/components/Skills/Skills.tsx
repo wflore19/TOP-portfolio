d68ad2449cbbdd7c0cdc6133b456a9018ee7e9a4
Will Flores <willflores1221@gmail.com>
2022-08-14 19:20:24 -0400

    Fix skills

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -45,7 +45,7 @@ export default function Skills() {
 						</svg>{" "}
 						Javascript
 					</li>
-					<li className="poo">
+					{/* <li className="poo">
 						<svg
 							style={{ width: 20, height: 20, margin: 0 }}
 							viewBox="0 0 24 24"
@@ -62,7 +62,7 @@ export default function Skills() {
 							<path d="M1.5 0h21l-1.91 21.563L11.977 24l-8.565-2.438L1.5 0zm17.09 4.413L5.41 4.41l.213 2.622 10.125.002-.255 2.716h-6.64l.24 2.573h6.182l-.366 3.523-2.91.804-2.956-.81-.188-2.11h-2.61l.29 3.855L12 19.288l5.373-1.53L18.59 4.414z" />
 						</svg>{" "}
 						CSS3
-					</li>
+					</li> */}
 				</ul>
 				<ul className="frameworks-databases">
 					<li className="list-title">
@@ -120,8 +120,6 @@ export default function Skills() {
 						</svg>{" "}
 						Jest
 					</li>
-					<li className="poo">Sanity CMS</li>
-					<li className="poo">Daisy UI</li>
 				</ul>
 				<ul className="tools">
 					<li className="list-title">
