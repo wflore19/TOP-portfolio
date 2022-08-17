d7daf5b034d3338b4437c8ebf02d951f13a91a68
wilfredo-flores <willflores1221@gmail.com>
2022-08-17 01:12:34 -0400

    Add profile picture

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -5,11 +5,10 @@ import NavBar from "../NavBar/NavBar";
 export default function Profile() {
 	return (
 		<div id="profile">
-			{/* <img src={pp} alt="wilfredo" /> */}
+			<img src={pp} alt="wilfredo" />
 			<div className="name">Wilfredo Flores</div>
 			<div className="title">Web Developer</div>
 			<NavBar />
-			<div className="cr">© 2022 All rights reserved</div>
 		</div>
 	);
 }
