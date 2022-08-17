371c09b0a911aa77e5446a546764edd3a78eb8e2
wilfredo-flores <willflores1221@gmail.com>
2022-08-17 01:12:04 -0400

    Fix imports

--- src/components/Mainframe/Mainframe.tsx
+++ src/components/Mainframe/Mainframe.tsx
@@ -3,7 +3,6 @@ import Heroes from "../Heroes/Heroes";
 import WhoAmI from "../WhoAmI/WhoAmI";
 import Skills from "../Skills/Skills";
 import Projects from "../Projects/Projects";
-import Resume from "../Resume/Experiences";
 
 export default function Mainframe() {
 	return (
